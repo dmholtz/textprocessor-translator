da80c259891c151fb9c7459af62f8ca6b2e20722
dmholtz <56723830+dmholtz@users.noreply.github.com>
2021-12-17 22:29:19 +0100

    Implement language detection

--- language-detection/app.js
+++ language-detection/app.js
@@ -1,32 +1,48 @@
 const { PubSub } = require('@google-cloud/pubsub');
 const pubsub = new PubSub();
 
-const translationTopic = pubsub.topic("tp-translator-Translation");
+const translationTopic = pubsub.topic("tp-translator-Translate");
 const saveTopic = pubsub.topic("tp-translator-Save");
 
-let publishTextToTopic = function (text, topic) {
-    const messageObject = {
-        data: {
-            message: text,
-        },
-    };
-    const messageBuffer = Buffer.from(JSON.stringify(messageObject), 'utf8');
+const { Translate } = require('@google-cloud/translate').v2;
+const translateClient = new Translate();
+
+async function publishTextToTopic(text, topic) {
+
+    const messageOptions = {
+        data: Buffer.from(JSON.stringify(text))
+    }
 
     try {
-        topic.publishMessage(messageBuffer);
-        console.log("Publish successful");
-    } catch (err) {
-        console.error(err);
+        const messageId = await topic.publishMessage(messageOptions);
+        console.log(`Message ${messageId} published.`);
+    } catch (error) {
+        console.error(`Received error while publishing; ${error.message}`);
     }
+}
+
+async function detectLanguage(text) {
+    console.log(`Detect language of text: ${text}.`);
+    let [detections] = await translateClient.detect(text);
+    // we expect only one detection, but to be future proof...
+    detections = Array.isArray(detections) ? detections : [detections];
+    detections.forEach(detection => {
+        console.log(`${detection.input} => detected ${detection.language} with confidence ${detection.confidence}`);
+    });
+    return detections[0].language;
+}
+
+const isEnglish = function (languageAcronym) {
+    return languageAcronym === 'en';
 }
 
 exports.languageDetection = function (message, context) {
     const text = message.data
         ? Buffer.from(message.data, 'base64').toString() : "undefined";
 
-    // detect language
-    let isEnglish = false;
-    if (isEnglish) {
+    const detectedLanguage = detectLanguage(text);
+
+    if (isEnglish(detectedLanguage)) {
         // save
         console.log('Publish to tp-translator-Save');
         publishTextToTopic(text, saveTopic)
@@ -36,4 +52,6 @@ exports.languageDetection = function (message, context) {
         console.log('Publish to tp-translator-Translation');
         publishTextToTopic(text, translationTopic)
     }
+
+    message.ack();
 }
