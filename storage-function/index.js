7eee47bcbfe5a3562b0e558dc1d010f1a4a92f94
dmholtz <56723830+dmholtz@users.noreply.github.com>
2021-12-18 11:58:49 +0100

    Add save module

--- storage-function/index.js
+++ storage-function/index.js
@@ -1,3 +1,25 @@
+const { Datastore } = require('@google-cloud/datastore');
+const datastore = new Datastore();
+
+const save = async function (text) {
+    // The kind for the new entity
+    const kind = 'Note';
+    // The name/ID for the new entity
+    const note = text;
+    // The Cloud Datastore key for the new entity
+    const taskKey = datastore.key([kind, note]);
+    // Prepares the new entity
+    const task = {
+        key: taskKey,
+        data: {
+            text: text,
+        },
+    };
+    // Saves the entity
+    await datastore.save(task);
+    console.log(`Saved ${task.key.name}: ${task.data.text}`);
+}
+
 /**
  * Background Cloud Function to be triggered by Pub/Sub.
  * Saves a message from the trigger topic to the Google Datastore.
@@ -5,9 +27,10 @@
  * @param {*} message 
  * @param {*} context 
  */
-exports.saveToDatastore = function (message, context) {
-    const content = message.data
+exports.saveToDatastore = async function (message, context) {
+    const text = message.data
         ? Buffer.from(message.data, 'base64').toString() : "undefined";
 
-    console.log('[INFO]: Received ' + content);
+    console.log('[INFO]: Save to datastore: ' + text);
+    save(text);
 }
